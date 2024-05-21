21fcd36e267a456eae253cfcdd6a26a809164afd
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-21 16:04:57 +0530

    BubbleSort using Do While loop change code

--- bubbleSort.js
+++ bubbleSort.js
@@ -7,6 +7,7 @@ function bubbleSort(arr){
 for(let i=0;i<arr.length;i++){
     for (let j=0; j<arr.length-1;j++){
         if(arr[j]>arr[j+1]){
+            swapped =true;
             let temp = arr[j];
             arr[j] =arr[j+1];
             arr[j+1]=temp;
